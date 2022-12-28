9e060730d581fd64df5114ac476976788bddd13c
zhuxingyuan <>
2022-12-28 09:26:05 +0800

    uupdate

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,13 +1,13 @@
-declare namespace dipUtils {
-    /**
-     * 生成数字范围内的随机数
-     * @param min 最小数字
-     * @param max 最大数字
-     * @returns number类型
-     */
-    export function random(min: number, max: number): number
+declare namespace ARRSixUtils {
+  /**
+   * 生成数字范围内的随机数
+   * @param min 最小数字
+   * @param max 最大数字
+   * @returns number类型
+   */
+  export function random(min: number, max: number): number
 }
 
-declare module 'dip-utils' {
-    export = dipUtils
+declare module 'arr-six-utils' {
+  export = ARRSixUtils
 }
